21c10ab85cbfa0d80c95b41604439986e1d796ae
Faizaan <ss16161@my.bristol.ac.uk>
2018-10-18 12:11:39 +0100

    Revert float to int

--- stencil.c
+++ stencil.c
@@ -58,7 +58,7 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
 
   // change float to int?? * 0.1 >> / 10 and
   float centreWeighting    = 0.6; // 3.0/5.0
-  int   neighbourWeighting = 10; // 0.5/5.0
+  int   neighbourWeighting = 0.1; // 0.5/5.0
 
   int xBound = nx - 1;
   int yBound = ny - 1;
@@ -70,10 +70,10 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
       int coord = j + (i * ny);
 
       tmp_image[coord]                  = image[coord]        * centreWeighting;
-      if (i > 0)      tmp_image[coord] += image[j + (i-1)*ny] / neighbourWeighting;
-      if (i < xBound) tmp_image[coord] += image[j + (i+1)*ny] / neighbourWeighting;
-      if (j > 0)      tmp_image[coord] += image[j - 1 + i*ny] / neighbourWeighting;
-      if (j < yBound) tmp_image[coord] += image[j + 1 + i*ny] / neighbourWeighting;
+      if (i > 0)      tmp_image[coord] += image[j + (i-1)*ny] * neighbourWeighting;
+      if (i < xBound) tmp_image[coord] += image[j + (i+1)*ny] * neighbourWeighting;
+      if (j > 0)      tmp_image[coord] += image[j - 1 + i*ny] * neighbourWeighting;
+      if (j < yBound) tmp_image[coord] += image[j + 1 + i*ny] * neighbourWeighting;
     }
   }
 
