3dc8b1dbfb3e56831811b06da6c0c9fff4faa786
Faizaan <ss16161@my.bristol.ac.uk>
2018-10-23 14:21:13 +0100

    streamlined instructions

--- stencil.c
+++ stencil.c
@@ -61,27 +61,19 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   for (int j = 0; j < 1; ++j) {
 
     // top left
-    tmp_image[j]  = image[j]  * centreWeighting;
-    tmp_image[j] += image[j + 1]  * neighbourWeighting;
-    tmp_image[j] += image[j + nx]  * neighbourWeighting;
+    tmp_image[j]  = (image[j]  * centreWeighting) + (image[j + 1]  * neighbourWeighting) + (image[j + nx]  * neighbourWeighting);
 
     for (int i = 1; i < nx - 1; ++i) {
 
       // middle
-      tmp_image[i]  = image[i]  * centreWeighting;
-      tmp_image[i] += image[i - 1]  * neighbourWeighting;
-      tmp_image[i] += image[i + 1]  * neighbourWeighting;
-      tmp_image[i] += image[i + nx]  * neighbourWeighting;
+      tmp_image[i]  = (image[i]  * centreWeighting) + (image[i - 1]  * neighbourWeighting) + (image[i + 1]  * neighbourWeighting) + (image[i + nx]  * neighbourWeighting);
 
     }
 
     // top right
     int a = nx - 1;
 
-    tmp_image[a]  = image[a]  * centreWeighting;
-    tmp_image[a] += image[a - 1]  * neighbourWeighting;
-    tmp_image[a] += image[a + nx]  * neighbourWeighting;
-
+    tmp_image[a]  = (image[a]  * centreWeighting) + (image[a - 1]  * neighbourWeighting) + (image[a + nx]  * neighbourWeighting);
   }
 
   // loop for middle
@@ -90,30 +82,20 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
     // left column
     int b = j * nx;
 
-    tmp_image[b]  = image[b]  * centreWeighting;
-    tmp_image[b] += image[b + 1]  * neighbourWeighting;
-    tmp_image[b] += image[b + nx]  * neighbourWeighting;
-    tmp_image[b] += image[b - nx]  * neighbourWeighting;
+    tmp_image[b]  = (image[b]  * centreWeighting) + (image[b + 1]  * neighbourWeighting) + (image[b + nx]  * neighbourWeighting) + (image[b - nx]  * neighbourWeighting);
 
     for (int i = 1; i < nx - 1; ++i) {
 
       // middle
       int c = (j * nx) + i;
 
-      tmp_image[c]  = image[c]  * centreWeighting;
-      tmp_image[c] += image[c + 1]  * neighbourWeighting;
-      tmp_image[c] += image[c - 1]  * neighbourWeighting;
-      tmp_image[c] += image[c + nx]  * neighbourWeighting;
-      tmp_image[c] += image[c - nx]  * neighbourWeighting;
+      tmp_image[c]  = (image[c]  * centreWeighting) + (image[c + 1]  * neighbourWeighting) + (image[c - 1]  * neighbourWeighting) + (image[c + nx]  * neighbourWeighting) + (image[c - nx]  * neighbourWeighting);
     }
 
     // right column
     int d = (j * nx) + (nx - 1);
 
-    tmp_image[d]  = image[d]  * centreWeighting;
-    tmp_image[d] += image[d - 1]  * neighbourWeighting;
-    tmp_image[d] += image[d + nx]  * neighbourWeighting;
-    tmp_image[d] += image[d - nx]  * neighbourWeighting;
+    tmp_image[d]  = (image[d]  * centreWeighting) + (image[d - 1]  * neighbourWeighting) + (image[d + nx]  * neighbourWeighting) + (image[d - nx]  * neighbourWeighting);
   }
 
   // loop for bottom row
@@ -122,27 +104,20 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
     // bottom left
     int e = j * nx;
 
-    tmp_image[e]  = image[e]  * centreWeighting;
-    tmp_image[e] += image[e + 1]  * neighbourWeighting;
-    tmp_image[e] += image[e - nx]  * neighbourWeighting;
+    tmp_image[e]  = (image[e]  * centreWeighting) + (image[e + 1]  * neighbourWeighting) + (image[e - nx]  * neighbourWeighting);
 
 
     for (int i = 1; i < nx - 1; ++i) {
       // middle
       int f = (j * nx) + i;
 
-      tmp_image[f]  = image[f]  * centreWeighting;
-      tmp_image[f] += image[f - 1]  * neighbourWeighting;
-      tmp_image[f] += image[f + 1]  * neighbourWeighting;
-      tmp_image[f] += image[f - nx]  * neighbourWeighting;
+      tmp_image[f]  = (image[f]  * centreWeighting) + (image[f - 1]  * neighbourWeighting) + (image[f + 1]  * neighbourWeighting) + (image[f - nx]  * neighbourWeighting);
     }
 
     // bottom right
     int g = (nx * ny) - 1;
 
-    tmp_image[g]  = image[g]  * centreWeighting;
-    tmp_image[g] += image[g - 1]  * neighbourWeighting;
-    tmp_image[g] += image[g - nx]  * neighbourWeighting;
+    tmp_image[g]  = (image[g]  * centreWeighting) + (image[g - 1]  * neighbourWeighting) + (image[g - nx]  * neighbourWeighting);
   }
 
 
